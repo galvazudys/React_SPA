6919982ec0b59637320eff079a06854285015c36
galvazudys <golfas705@yahoo.com>
2017-04-19 17:56:07 +0100

    created routes and router comp

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 // Libs
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import { HashRouter } from 'react-router-dom';
 
-// CSS 
+// CSS
 import './css/style.css';
 
 // Components
 import App from './components/App';
+import Home from './components/Home';
+import Courses from './components/Courses';
+import About from './components/About';
+import Teachers from './components/Teachers'
 
 // Render
-render(
-  <App />,
-  document.getElementById('root')
-);
+render((
+    <HashRouter >
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/courses" component={Courses} />
+        <Route path="/teachers" component={Teachers} />
+      </div>
+    </HashRouter>    
+), document.getElementById('root'));
